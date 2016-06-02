659160de9dfd95ecfb4c10c34105eb1a2e3d1a94
belyaev-ay <fxdev-belyaev-ay@fxclub.org>
2016-06-02 09:30:31 +0700

    first commit

---
+++ src/main/java/com/avvero/sortman/App.java
@@ -0,0 +1,12 @@
+package com.avvero.sortman;
+
+/**
+ * @author fxdev-belyaev-ay
+ */
+public class App {
+
+    public static void main(String... args) {
+        System.out.println("hello world");
+    }
+
+}
---
+++ src/test/groovy/com/avvero/sortman/AppTests.groovy
@@ -0,0 +1,15 @@
+package com.avvero.sortman
+
+import spock.lang.Specification
+
+/**
+ * @author fxdev-belyaev-ay
+ */
+class AppTests extends Specification{
+
+    def "Simple"() {
+        expect:
+            1 == 1
+    }
+
+}
